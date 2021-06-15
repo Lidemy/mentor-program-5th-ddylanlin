4b67e433a736b941c82a06235151569e9980d903
Dylan <silence469@gmail.com>
2021-06-15 16:27:36 +0800

    Restaurant Combine

---
+++ homeworks/week10/Restaurant Web/lottery.js
@@ -0,0 +1,48 @@
+// 因為 Move function declaration to function body root. eslint(no-inner-declarations)
+// 所以改動了順序 但更不好閱讀!?
+const request = new XMLHttpRequest()
+request.onload = () => {
+  if (request.status >= 200 && request.status < 400) {
+    const response = request.responseText
+    const json = JSON.parse(response)
+    const result = json.prize
+    console.log(result)
+
+    document.querySelector('.announce-block').addEventListener('submit', (e) => {
+      if (e.target.classList.contains('announce-btn')) {
+        isAnnounce.classList.add('hide')
+        isMethod.previousElementSibling.classList.remove('hide')
+        background.classList.add('event-bg')
+      }
+    })
+
+    document.querySelector('.eventMethod-block').addEventListener('click', (e) => {
+      if (e.target.classList.contains('eventMethod-btn')) {
+        result ? choose(result) : alert('系統不穩定，請重新整理再試一次！（切換錯誤）')
+      }
+    })
+  } else {
+    console.log(request.status)
+    alert(`系統不穩定，請重新整理再試一次！statusCode: ${request.status}`)
+  }
+
+  const background = document.querySelector('.event-bg')
+  const isMethod = document.querySelector('.eventMethod-block')
+  const isAnnounce = document.querySelector('.announce-block')
+
+  // 切換頁面
+  function choose(result) {
+    isMethod.classList.add('hide')
+    isAnnounce.classList.remove('hide')
+    document.querySelector(`.${result}`).classList.remove('hide')
+    background.classList.remove('event-bg')
+    background.classList.add(`${result}-bg`)
+  }
+}
+request.onerror = () => {
+  console.log('error')
+  alert('系統不穩定，請重新整理再試一次！（onload失敗）')
+}
+
+request.open('GET', 'https://dvwhnbka7d.execute-api.us-east-1.amazonaws.com/default/lottery', true)
+request.send()
