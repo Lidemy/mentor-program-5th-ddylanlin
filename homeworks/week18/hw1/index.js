7358182d07dd866944cf645ba425f836f651191d
Dylan <silence469@gmail.com>
2021-08-11 00:56:24 +0800

    porting & HW4

---
+++ homeworks/week18/hw1/index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
+
+const app = express()
+const port = process.env.PORT || 5001
+
+// const userController = require('./controllers/user')
+// const articleController = require('./controllers/article')
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.errorMessage = req.flash('errorMessage')
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/faq', (req, res) => {
+  res.render('faq')
+})
+
+app.get('/lottery', (req, res) => {
+  res.render('lottery')
+})
+
+// app.get('/page', articleController.page)
+// app.get('/article/:id', articleController.view)
+// app.get('/create', articleController.create)
+// app.post('/create', articleController.handleCreate, redirectBack)
+// app.get('/delete/:id', articleController.delete)
+// app.get('/update/:id', articleController.update)
+// app.post('/update/:id', articleController.handleUpdate, redirectBack)
+// app.get('/admin', articleController.admin)
+// app.post('/admin', articleController.handleSwitchAdmin, redirectBack)
+// app.get('/recover/:id', articleController.handleRecover, redirectBack)
+
+app.listen(port, () => {
+  console.log(`♫.(◕∠◕).♫ http://localhost:${port}  ♫.(◕∠◕).♫`)
+})
---
+++ homeworks/week18/hw1/public/scripts/lottery.js
@@ -0,0 +1,39 @@
+const background = document.querySelector('.event-bg')
+const isMethod = document.querySelector('.eventMethod-block')
+const isAnnounce = document.querySelector('.announce-block')
+
+async function lotteryData() {
+  try {
+    const response = await fetch('https://lit-stream-79960.herokuapp.com/lottery')
+    if (!response.ok) {
+      console.log(response.status)
+      alert(`系統不穩定，請再試一次！statusCode: ${response.status}`)
+    }
+    return await response.json()
+  } catch (err) {
+    console.log('error', err)
+    alert('系統不穩定！（heroku request失敗）')
+  }
+}
+
+async function renderResult() {
+  const result = await lotteryData()
+  document.querySelector('.lottery-result').innerText = result.prize
+  document.querySelector('.announce-desc').innerText = result.description
+  isMethod.classList.add('hide')
+  isAnnounce.classList.remove('hide')
+  background.style.backgroundImage = `url(${result.imageURL})`
+}
+
+document.querySelector('.eventMethod-block').addEventListener('click', (e) => {
+  if (e.target.classList.contains('eventMethod-btn')) {
+    renderResult()
+  }
+})
+
+document.querySelector('.announce-block').addEventListener('submit', (e) => {
+  if (e.target.classList.contains('announce-btn')) {
+    isAnnounce.classList.add('hide')
+    background.classList.add('event-bg')
+  }
+})
